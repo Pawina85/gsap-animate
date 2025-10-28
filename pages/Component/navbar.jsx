18a4eb9f6d25c900f26aac6f8d194addf6d38624
Pawina85 <pawinachanthachon@gmail.com>
2025-10-28 22:35:14 +0700

    Convert project from TypeScript to JSX and fix routing conflicts
    - Remove App Router (app/) to eliminate routing conflicts with Pages Router
    - Convert all .js files to .jsx for consistency
    - Remove TypeScript dependencies and configuration files
    - Update Next.js config to use JavaScript instead of TypeScript
    - Add Navbar component with logo image support
    - Fix image path for PlantsLogo.png in navbar
    - Clean up package.json dependencies

---
+++ pages/Component/navbar.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Navbar() {
+    return (
+        <nav>            
+        <div>
+        <a href="home">
+         <img src="/images/PlantsLogo.png" alt="logo" />
+        </a>
+        </div>
+        </nav>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Navbar from './Component/navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
