156572cf1a8d3864b90977e5cc452dcbe873c5d3
Pawina85 <pawinachanthachon@gmail.com>
2025-10-30 23:13:32 +0700

    add gallery

--- constants/index.js
+++ constants/index.js
@@ -16,6 +16,38 @@ const navLinks = [
     title: "Cart / Login",
     },
 ]
+const gallerylist = [
+    {
+    name: "Fiddle Leaf Fig",
+    price: "$45",
+    image: "/images/FiddleLeafFig.png",
+    description: "Large glossy leaves that bring modern jungle vibes indoors."
+  },
+  {
+    name: "Bird of Paradise",
+    price: "$65",
+    image: "/images/BirdofParadise.png",
+    description: "Tropical beauty with tall, sculptural leaves for bright rooms."
+  },
+  {
+    name: "Rubber Plant",
+    price: "$35",
+    image: "/images/RubberPlant.png",
+    description: "Bold dark foliage that thrives in minimal modern spaces."
+  },
+  {
+    name: "Olive Tree",
+    price: "$50",
+    image: "/images/olive-tree.jpg",
+    description: "Soft silver-green leaves that bring Mediterranean calm."
+  },
+  {
+    name: "Money Tree",
+    price: "$40",
+    image: "/images/MoneyPlant.png",
+    description: "Symbol of good fortune and a refreshing indoor accent."
+  },
+]
 
 
-export { navLinks };
+export { navLinks, gallerylist };
