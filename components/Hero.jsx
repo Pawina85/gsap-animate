94a6acb09c170fb5db5107a0737ef9685c3c3fc7
Pawina85 <pawinachanthachon@gmail.com>
2025-11-05 22:21:00 +0700

    Create components

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Hero() {
+    return (
+        <section>
+            <div>
+                <h1>Fuck up the world!!</h1>
+            </div>
+        </section>
+    )
+}
